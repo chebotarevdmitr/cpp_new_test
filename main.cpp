1127b93a05945eb30bd321fbbb0751354a293a11
Dmitriy Chebotarev <4ebotarov_dim@inbox.ru>
2026-03-15 02:04:06 +0300

    input resul

--- main.cpp
+++ main.cpp
@@ -1,14 +1,19 @@
 #include <iostream>
 
-consteval int get_value()
+int main([[maybe_unused]] int argc, [[maybe_unused]] char const *argv[])
 {
-  return 3;
-}
+  int numb1 = 0;
+  int numb2 = 0;
 
-int main(int argc, char const *argv[])
-{
-  constexpr int value = get_value();
-  std::cout << "value = " << value << std::endl;
+  std::cout << "Enter numb1: ";
+  std::cin >> numb1;
+
+  std::cout << "Enter numb2: ";
+  std::cin >> numb2;
+
+  int res = numb1 + numb2;
+
+  std::cout << "result = " << res << std::endl;
 
   return 0;
 }
