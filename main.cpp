ef8f55a429fcb4f65ac48202ecc0d78d0b92d398
Dmitriy Chebotarev <4ebotarov_dim@inbox.ru>
2026-03-17 22:39:46 +0300

    for Dmitriy

--- main.cpp
+++ main.cpp
@@ -1,19 +1,12 @@
 #include <iostream>
+#include <string>
 
-int main([[maybe_unused]] int argc, [[maybe_unused]] char const *argv[])
+int main([[maybe_unused]] int argc, [[maybe_unused]] char const*argv[])
 {
-  int numb1 = 0;
-  int numb2 = 0;
-
-  std::cout << "Enter numb1: ";
-  std::cin >> numb1;
-
-  std::cout << "Enter numb2: ";
-  std::cin >> numb2;
-
-  int res = numb1 + numb2;
-
-  std::cout << "result = " << res << std::endl;
-
+  std::string name = "Dmitriy";
+  for (int i=0;i<10;++i)
+  {
+  std::cout << "number 1 = " << name << std::endl;
+  }
   return 0;
 }
